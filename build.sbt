ac43e297636579d0da75249870323b4afc0c7888
Franco Testagrossa <franco@tiendanube.com>
2019-06-08 19:36:54 -0300

    Improving akka/k8s config approach

--- build.sbt
+++ build.sbt
@@ -2,31 +2,32 @@ name := "akka-simple-cluster-k8s"
 
 version := "0.0.1-SNAPSHOT"
 
-scalaVersion := "2.12.6"
+scalaVersion := "2.12.8"
 
 resolvers += Resolver.bintrayRepo("tanukkii007", "maven")
 
 enablePlugins(JavaServerAppPackaging, DockerPlugin)
 
-val akkaVersion = "2.5.17"
-val akkaHttpVersion = "10.1.5"
-val akkaManagementVersion = "0.18.0"
+val akkaVersion = "2.5.23"
+val akkaHttpVersion = "10.1.7"
+val akkaManagementVersion = "1.0.0"
 
 libraryDependencies ++=Seq(
-  "com.typesafe.akka" %% "akka-persistence" % akkaVersion,
-  "com.typesafe.akka" %% "akka-persistence" % akkaVersion,
-  "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
-  "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion,
-  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
   "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
   "com.typesafe.akka" %% "akka-cluster-sharding" % akkaVersion,
-  "com.lightbend.akka.discovery" %% "akka-discovery-kubernetes-api" % akkaManagementVersion,
+  "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
+  "com.typesafe.akka" %% "akka-remote" % akkaVersion,
+  "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+  "com.typesafe.akka" %% "akka-discovery" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion,
   "com.lightbend.akka.management" %% "akka-management" % akkaManagementVersion,
   "com.lightbend.akka.management" %% "akka-management-cluster-http" % akkaManagementVersion,
-  "com.lightbend.akka.management" %% "akka-management-cluster-bootstrap" % akkaManagementVersion,
-  "com.lightbend.akka.discovery" %% "akka-discovery-dns" % akkaManagementVersion,
+  "com.lightbend.akka.management" %% "akka-management-cluster-bootstrap" % akkaManagementVersion,  
   "com.github.TanUkkii007" %% "akka-cluster-custom-downing" % "0.0.12"
 )
+
+libraryDependencies += "com.twitter" %% "chill-akka" % "0.9.3" // for kryo
 
 dockerBaseImage := "openjdk:8"
 dockerUsername := Some("softwaremill")
--- src/main/scala/com/softwaremill/akkaSimpleCluster/Main.scala
+++ src/main/scala/com/softwaremill/akkaSimpleCluster/Main.scala
@@ -2,12 +2,15 @@ package com.softwaremill.akkaSimpleCluster
 
 import akka.actor.ActorSystem
 import akka.cluster.Cluster
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
 import akka.management.AkkaManagement
 import akka.management.cluster.bootstrap.ClusterBootstrap
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.util.{Failure, Success}
 
 
 object Main extends App {
@@ -16,10 +19,42 @@ object Main extends App {
   implicit val system = ActorSystem("akka-simple-cluster")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
-  implicit val cluster = Cluster(system)
+  implicit val cluster = Cluster(system) // ??
 
   AkkaManagement(system).start()
   ClusterBootstrap(system).start()
 
-  Http().bindAndHandle(complete(config.getString("application.api.hello-message")), config.getString("application.api.host"), config.getInt("application.api.port"))
+  Http().bindAndHandle(
+    complete(config.getString("application.api.hello-message")),
+    config.getString("application.api.host"),
+    config.getInt("application.api.port")
+  )
+
+  // exitSystemOnTerminate(system)
+
+  private def exitSystemOnTerminate(actorSystem: ActorSystem): Unit = {
+
+    implicit val dispatcher: ExecutionContextExecutor = actorSystem.dispatcher
+    val cluster = Cluster.get(actorSystem)
+
+    cluster.registerOnMemberRemoved {
+      actorSystem.whenTerminated onComplete {
+        case Success(_) =>
+          // logger.info(s"System exit after ${actorSystem.name} terminate.")
+          System.exit(0)
+        case Failure(_) =>
+          // logger.warn(s"${actorSystem.name} abnormally termination")
+          System.exit(-1)
+      }
+    }
+
+    Runtime.getRuntime.addShutdownHook(new Thread() {
+      override def run(): Unit = {
+        // logger.info("Downing {} from Cluster...", cluster.selfAddress)
+        cluster.down(cluster.selfAddress)
+        // actorSystem.registerOnTermination(logger.info("System shut down."))
+        actorSystem.terminate()
+      }
+    })
+  }
 }
